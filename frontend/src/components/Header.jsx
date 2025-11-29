d237761037b73eac83b437368f9af04d44b57f0b
Александр Кирилюк <evilbald@Mac.fritz.box>
2025-11-30 00:55:43 +0300

    Increase logo container size

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,8 +6,8 @@ export default function Header({ user }) {
       <div className="header-main-row">
         <div className="logo">
           {/* New Alfa A Logo */}
-          <div className="logo-icon">
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" style={{overflow: 'visible'}}>
+          <div className="logo-icon" style={{width: '40px', height: '40px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <svg width="36" height="36" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" style={{overflow: 'visible'}}>
               <path d="M16 4L4 28H10L16 16L22 28H28L16 4Z" fill="#FF0000"/>
               <rect x="7" y="22" width="18" height="4" fill="#FF0000"/>
             </svg>
