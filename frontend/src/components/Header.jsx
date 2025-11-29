f782c180c88b858edae2a75edd9293ced5429976
Александр Кирилюк <evilbald@Mac.fritz.box>
2025-11-30 00:52:25 +0300

    Fix logo clipping with overflow visible

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ user }) {
         <div className="logo">
           {/* New Alfa A Logo */}
           <div className="logo-icon">
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" style={{overflow: 'visible'}}>
               <path d="M16 4L4 28H10L16 16L22 28H28L16 4Z" fill="#FF0000"/>
               <rect x="7" y="22" width="18" height="4" fill="#FF0000"/>
             </svg>
